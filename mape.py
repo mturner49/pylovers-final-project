4a970b8cc5f454158cb0e0418c4e933c145f8c1b
Vida-L <74931596+Vida-L@users.noreply.github.com>
2020-12-09 01:32:25 -0500

    Include all the plots

--- mape.py
+++ mape.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-p = pd.read_csv('sample_submission.csv')
+p = pd.read_csv('pylover_submission.csv')
 o = pd.read_csv('test_actual_price.csv')
 eva = pd.merge(p, o, on='Id', how='inner', suffixes=['_pred', '_actual'])
 mape = np.mean(np.abs(eva['SalePrice_actual'] - eva['SalePrice_pred']) / eva['SalePrice_actual'])
